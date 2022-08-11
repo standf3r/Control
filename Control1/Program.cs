50f34ae269d0f0005b389261ada0333d3d052a65
standfer <vertigl@yandex.ru>
2022-08-11 21:28:58 +0800

    Добавили алгоритм решения

--- Control1/Program.cs
+++ Control1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿void stringArrayInNewArray(string[] stringArray, int n)
+{
+    for (int i = 0; i < stringArray.Length; i++) // вывод в консоль первоначального массива
+    {
+        Console.Write(stringArray[i] + " ");
+    }
+    Console.Write(" => ");
+        
+    string tempStr; // перменная для присваивания элемента массива строки
+    char[] tempChars; // переменная для присваивания символов из массива строки (знаков)
+    for (int i = 0; i < stringArray.Length; i++) // формирование нового массива и вывод его в консоль
+    {
+        tempStr = stringArray[i]; // присваивание i элемента
+        tempChars = tempStr.ToCharArray(); // присваивание знаков элемента
+        if (tempChars.Length <= n) // условие сравнения количества знаков в элементе строки
+        {
+        Console.Write(stringArray[i] + " "); // вывод в консоль
+        }    
+    }
+    Console.WriteLine();
+}
